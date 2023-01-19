befb744ee1ea23142681db185a40b0bd115780e6
YB <1024364354@qq.com>
2023-01-19 21:03:36 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,147 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import static org.junit.Assert.*;
 
 public class Percolation {
+    private class Site {
+        boolean full;
+        boolean open;
+        boolean connectedToBottom;
+        int index;
+        Site(int x, int y, int N) {
+            full = false;
+            open = false;
+            connectedToBottom = false;
+            index = y + x * N;
+        }
+    }
+    private boolean percolation;
+    private int size;
+    private Site[][] grid;
+    private int openSites;
+    private WeightedQuickUnionUF gridUnion;
 
+    // create N-by-N grid, with all Sites initially blocked
+    public Percolation(int N) {
+        size = N;
+        percolation = false;
+        grid = new Site[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = new Site(i, j, N);
+            }
+        }
+        openSites = 0;
+        gridUnion = new WeightedQuickUnionUF(N * N);
+    }
+
+    private Site represent(int index) {
+        int repre = gridUnion.find(index);
+        return grid[repre / size][repre % size];
+    }
+
+    private void connect(Site a, Site b) {
+        if (a.open && b.open) {
+            boolean isfull = represent(a.index).full || represent(b.index).full;
+            boolean atbottom = represent(a.index).connectedToBottom
+                    || represent(b.index).connectedToBottom;
+            gridUnion.union(a.index, b.index);
+            represent(a.index).full = isfull;
+            represent(a.index).connectedToBottom = atbottom;
+            if (isfull && atbottom) {
+                percolation = true;
+            }
+        }
+    }
+    // open the Site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of range");
+        }
+        if (grid[row][col].open) {
+            return;
+        }
+        grid[row][col].open = true;
+        openSites++;
+        if (row == 0) {
+            grid[row][col].full = true;
+        }
+        if (row == size - 1) {
+            grid[row][col].connectedToBottom = true;
+        }
+        if (row > 0) {
+            connect(grid[row - 1][col], grid[row][col]);
+        }
+        if (row < size - 1) {
+            connect(grid[row + 1][col], grid[row][col]);
+        }
+        if (col > 0) {
+            connect(grid[row][col - 1], grid[row][col]);
+        }
+        if (col < size - 1) {
+            connect(grid[row][col + 1], grid[row][col]);
+        }
+    }
+
+    // is the Site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of range");
+        }
+        return grid[row][col].open;
+    }
+
+    // is the Site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException("row or col is out of range");
+        }
+        return represent(grid[row][col].index).full;
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return percolation;
+    }
+
+    public static void main(String[] args) {
+        Percolation model = new Percolation(10);
+        for (int i = 0; i < 9; i++) {
+            model.open(i, 0);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            assertTrue(model.isOpen(i, 0));
+            assertTrue(model.isFull(i, 0));
+        }
+        assertTrue(!model.percolates());
+        model.open(9, 0);
+        assertTrue(model.percolates());
+        assertTrue(model.numberOfOpenSites() == 10);
+
+        Percolation model1 = new Percolation(20);
+        for (int i = 0; i < 19; i++) {
+            model1.open(i, 0);
+        }
+        for (int i = 0; i < 19; i++) {
+            assertTrue(model1.isOpen(i, 0));
+            assertTrue(model1.isFull(i, 0));
+        }
+        for (int i = 1; i < 20; i++) {
+            model1.open(i, 2);
+        }
+        for (int i = 1; i < 20; i++) {
+            assertTrue(model1.isOpen(i, 2));
+            assertTrue(!model1.isFull(i, 2));
+        }
+        assertTrue(!model1.percolates());
+        model1.open(10, 1);
+        assertTrue(model1.isFull(10, 1));
+        assertTrue(model1.percolates());
+        assertTrue(model1.numberOfOpenSites() == 39);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,37 @@
 package hw2;
 
+import static edu.princeton.cs.introcs.StdRandom.*;
+
 public class PercolationStats {
+    private int[] threshold;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("args should be positive");
+        }
+        threshold = new int[T];
+        for (int i = 0; i < T; i++) {
+            Percolation model = pf.make(N);
+            while (!model.percolates()) {
+                model.open(uniform(N), uniform(N));
+            }
+            threshold[T] = model.numberOfOpenSites();
+        }
+    }
+
+    public double mean() {
+        return edu.princeton.cs.introcs.StdStats.mean(threshold);
+    }
+
+    public double stddev() {
+        return edu.princeton.cs.introcs.StdStats.stddev(threshold);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(threshold.length);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(threshold.length);
+    }
 
 }
